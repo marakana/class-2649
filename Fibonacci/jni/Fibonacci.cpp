5701c6b3bc9064ddba1947ab85f1e27a0728881b
Marko Gargenta <marko@marakana.com>
2012-08-29 12:29:06 -0700

    Implemented simple UI, fixed bug in CPP code

--- Fibonacci/jni/Fibonacci.cpp
+++ Fibonacci/jni/Fibonacci.cpp
@@ -2,10 +2,14 @@
 
 namespace com_intel_fibonacci {
 
-	static jlong fibN(jlong n) {
+	static jlong fib(jlong n) {
 		if(n==0) return 0;
 		if(n==1) return 1;
-		return fibN(n-1)+fibN(n-2);
+		return fib(n-1)+fib(n-2);
+	}
+
+	static jlong fibN(JNIEnv *env, jclass clazz, jlong n){
+		return fib(n);
 	}
 
 	static JNINativeMethod method_table[] = {
--- Fibonacci/src/com/intel/fibonacci/MainActivity.java
+++ Fibonacci/src/com/intel/fibonacci/MainActivity.java
@@ -1,20 +1,34 @@
 package com.intel.fibonacci;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	EditText editN;
+	TextView textOut;
+ 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        editN = (EditText) findViewById(R.id.edit_n);
+        textOut = (TextView) findViewById(R.id.text_out);
+    }
+    
+    /** Called when button_go is clicked on. */
+    public void onClickButtonGo(View v) {
+    		long n = Long.parseLong( editN.getText().toString() );
+    		
+    		long resultJ = FibLib.fibJ(n);
+    		textOut.append( String.format("\nfibJ(%d)=%d", n, resultJ) );
+    		
+    		long resultN = FibLib.fibN(n);
+    		textOut.append( String.format("\nfibN(%d)=%d", n, resultN) );
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
 }
