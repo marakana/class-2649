17f2ced6b673e7251b7918a81fdae01229be8974
Marko Gargenta <marko@marakana.com>
2012-08-29 11:19:47 -0700

    Implemented CPP version of Fibonacci Library

--- Fibonacci/jni/Android.mk
+++ Fibonacci/jni/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := Fibonacci
-LOCAL_SRC_FILES := Fibonacci.c
+LOCAL_SRC_FILES := Fibonacci.cpp
 
 include $(BUILD_SHARED_LIBRARY)
--- Fibonacci/jni/Fibonacci.cpp
+++ Fibonacci/jni/Fibonacci.cpp
@@ -1,1 +1,32 @@
 #include <jni.h>
+
+namespace com_intel_fibonacci {
+
+	static jlong fib(jlong n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return fib(n-1)+fib(n-2);
+	}
+
+	static JNINativeMethod method_table[] = {
+		{ "fib", "(J)J", (void *) fib }
+	 };
+}
+
+using namespace com_intel_fibonacci;
+
+extern "C" jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env;
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return -1;
+    } else {
+        jclass clazz = env->FindClass("com/intel/fibonacci/FibLib");
+        if (clazz) {
+            env->RegisterNatives(clazz, method_table, sizeof(method_table) / sizeof(method_table[0]));
+            env->DeleteLocalRef(clazz);
+            return JNI_VERSION_1_6;
+        } else {
+            return -1;
+        }
+    }
+}
