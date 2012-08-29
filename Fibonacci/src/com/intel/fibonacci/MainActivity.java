b60a4f0994169dcef7c681dab7be8eb886f853ec
Marko Gargenta <marko@marakana.com>
2012-08-29 14:13:03 -0700

    Implemented timers

--- Fibonacci/src/com/intel/fibonacci/MainActivity.java
+++ Fibonacci/src/com/intel/fibonacci/MainActivity.java
@@ -9,26 +9,39 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	EditText editN;
 	TextView textOut;
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        editN = (EditText) findViewById(R.id.edit_n);
-        textOut = (TextView) findViewById(R.id.text_out);
-    }
-    
-    /** Called when button_go is clicked on. */
-    public void onClickButtonGo(View v) {
-    		long n = Long.parseLong( editN.getText().toString() );
-    		
-    		long resultJ = FibLib.fibJ(n);
-    		textOut.append( String.format("\nfibJ(%d)=%d", n, resultJ) );
-    		
-    		long resultN = FibLib.fibN(n);
-    		textOut.append( String.format("\nfibN(%d)=%d", n, resultN) );
 
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		editN = (EditText) findViewById(R.id.edit_n);
+		textOut = (TextView) findViewById(R.id.text_out);
+	}
+
+	static final int LOOP = 10;
+
+	/** Called when button_go is clicked on. */
+	public void onClickButtonGo(View v) {
+		long n = Long.parseLong(editN.getText().toString());
+
+		long resultJ = 0, resultN = 0;
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < LOOP; i++) {
+			resultJ = FibLib.fibJ(n);
+		}
+		long timeJ = System.currentTimeMillis() - start;
+		textOut.append(String.format("\nfibJ(%d)=%d (~%d ms)", n, resultJ,
+				timeJ / LOOP));
+
+		start = System.currentTimeMillis();
+		for (int i = 0; i < LOOP; i++) {
+			resultN = FibLib.fibN(n);
+		}
+		long timeN = System.currentTimeMillis() - start;
+		textOut.append(String.format("\nfibN(%d)=%d (~%d ms)", n, resultN,
+				timeN / LOOP));
+
+	}
 
 }
